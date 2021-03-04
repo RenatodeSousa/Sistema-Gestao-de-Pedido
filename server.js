358b4507412735c545fd3f4b5783653f80654ffa
Renato Sousa <renatodesousadasilva.1@gmail.com>
2021-03-04 16:40:23 -0300

    heroku

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/Gestao-de-Pedidos'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/Gestao-de-Pedidos/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080,
+  () => console.log("Server is running..."));
