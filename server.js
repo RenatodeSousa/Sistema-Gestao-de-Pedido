96e5f0e85ba94f3f01f55f05f82c666209e76b3e
Renato Sousa <renatodesousadasilva.1@gmail.com>
2021-03-10 16:51:14 -0300

    heroku

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ app.use(express.static(__dirname + '/dist/'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-app.listen(process.env.PORT || 4200,
+app.listen(process.env.PORT || 8080,
   () => console.log("Server is running...", process.env.PORT ));
