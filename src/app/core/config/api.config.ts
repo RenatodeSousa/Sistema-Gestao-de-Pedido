1d9819b08384f542ccf125f072b7c33b7af21a5a
Renato Sousa <renatodesousadasilva.1@gmail.com>
2021-05-26 15:20:56 -0300

    alterar servidor

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-   baseUrl: 'http://localhost:8080',
- // baseUrl: 'https://controle-pedido.herokuapp.com',
+   //baseUrl: 'http://localhost:8080',
+  baseUrl: 'https://controle-pedido.herokuapp.com',
   bucketBaseUrl: 'https://marmoraria.s3.sa-east-1.amazonaws.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   URL_REQUEST: "http://localhost:4200",
 };
 export const API_CONFIG = {
-  baseUrl: 'http://localhost:8080',
-  // baseUrl: 'https://controle-pedido.herokuapp.com',
+  //baseUrl: 'http://localhost:8080',
+   baseUrl: 'https://controle-pedido.herokuapp.com',
   bucketBaseUrl: 'https://marmoraria.s3.sa-east-1.amazonaws.com',
 };
 
